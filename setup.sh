932272935508d7c184a5bf79f06e1d1e533ff7f8
Renaud Gaubert <rgaubert@nvidia.com>
2019-09-23 16:27:26 -0700

    Update calico version
    Signed-off-by: Renaud Gaubert <rgaubert@nvidia.com>

--- setup.sh
+++ setup.sh
@@ -18,8 +18,7 @@ EOF
 	mkdir -p /home/ubuntu/.kube
 	cp /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
 	chown ubuntu:ubuntu /home/ubuntu/.kube/config
-	kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
-	kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+	kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
 	kubectl taint nodes --all node-role.kubernetes.io/master-
 }
 
