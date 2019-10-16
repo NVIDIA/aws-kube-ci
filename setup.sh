23c1fdcf0a2208d088ede0ac1e3c7d191e943083
guptaNswati <swatig@nvidia.com>
2019-10-16 12:15:48 -0700

    Update calico version

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ EOF
 	mkdir -p /home/ubuntu/.kube
 	cp /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
 	chown ubuntu:ubuntu /home/ubuntu/.kube/config
-	kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
+	kubectl apply -f https://docs.projectcalico.org/v3.9/manifests/calico.yaml
 	kubectl taint nodes --all node-role.kubernetes.io/master-
 }
 
