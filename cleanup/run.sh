2aa495de74fde90229cf88851669f69db32a8ab4
Renaud Gaubert <rgaubert@nvidia.com>
2020-05-05 17:05:13 -0700

    Add cleanup stage
    Signed-off-by: Renaud Gaubert <rgaubert@nvidia.com>

---
+++ cleanup/run.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+instances() {
+	aws ec2 describe-instances --filters "Name=tag:product,Values=cloud-native" \
+					     "Name=tag:environment,Values=cicd" \
+		--query "Reservations[].Instances[?LaunchTime>='$(date --date='-2 hours' '+%Y-%m-%dT%H:%M')'].InstanceId" \
+		--output text
+}
+
+instances="$(instances)"
+echo "Found the following instances to cleanup: $instances"
+
+for instance in $instances; do
+	# Note we don't filter out terminated instances, so the above command can fail
+	echo "Deleting instance: $instance"
+	aws ec2 delete-instance "$instance" || true
+done
