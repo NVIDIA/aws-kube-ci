bebd5c1d5393aafb39fe1d93709e53b60745987b
Carlos Eduardo Arango Gutierrez <eduardoa@nvidia.com>
2023-06-23 17:27:20 +0200

    Update tests to recent changes
    Signed-off-by: Carlos Eduardo Arango Gutierrez <eduardoa@nvidia.com>

--- tests/test_setup.sh
+++ tests/test_setup.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 
 source ../aws-kube-ci/hostname
+
+# Docker
+if [ "$CONTAINER_RUNTIME" = "docker" ]; then
 ssh -i ../aws-kube-ci/key -o StrictHostKeyChecking=no ${instance_hostname} \
-	"docker run containerdevk8s/cuda:10.0-nbody-ubuntu16.04 nvidia-smi"
-ssh -i ../aws-kube-ci/key ${instance_hostname} \
-  "docker run containerdevk8s/cuda:10.0-nbody-ubuntu16.04 0_Simple/vectorAdd/vectorAdd"
-ssh -i ../aws-kube-ci/key ${instance_hostname} \
-  "docker run containerdevk8s/cuda:10.0-nbody-ubuntu16.04 0_Simple/vectorAddDrv/vectorAddDrv"
+  "docker version"
+ssh -i ../aws-kube-ci/key -o StrictHostKeyChecking=no ${instance_hostname} \
+  "docker run hello-world"  
+fi
+# Containerd
+if [ "$CONTAINER_RUNTIME" = "containerd" ]; then
+ssh -i ../aws-kube-ci/key -o StrictHostKeyChecking=no ${instance_hostname} \
+  "containerd --version"  
+fi
+# CRI-O
+if [ "$CONTAINER_RUNTIME" = "crio" ]; then
+ssh -i ../aws-kube-ci/key -o StrictHostKeyChecking=no ${instance_hostname} \
+  "crio --version"  
+fi
+# Kubernetes
+if [ "$KUBERNETES_ENABLED" = "true" ]; then
+ssh -i ../aws-kube-ci/key -o StrictHostKeyChecking=no ${instance_hostname} \
+  "kubectl cluster-info && kubectl version --client"
+ssh -i ../aws-kube-ci/key -o StrictHostKeyChecking=no ${instance_hostname} \
+  "kubectl get nodes -o json | jq '.items[].status.nodeInfo.containerRuntimeVersion'"  
+fi
