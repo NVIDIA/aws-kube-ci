a7fa468cc94255f1612696a3995a4a4c575c7490
Carlos Eduardo Arango Gutierrez <eduardoa@nvidia.com>
2023-06-23 14:00:40 +0200

    Add CRI-O runtime
    Signed-off-by: Carlos Eduardo Arango Gutierrez <eduardoa@nvidia.com>

--- config/common.sh
+++ config/common.sh
@@ -30,3 +30,33 @@ with_retry() {
 
 	return 1
 }
+
+# Detect architecture
+architecture=$(uname -m)
+if [[ $architecture == "x86_64" || $architecture == "amd64" ]]; then
+    ARCH="amd64"
+elif [[ $architecture == "aarch64" || $architecture == "arm64" ]]; then
+    ARCH="arm64"
+else
+    echo "Unknown architecture: $architecture"
+    exit 1
+fi
+
+# Check if the operating system is Ubuntu
+if [ -f "/etc/os-release" ]; then
+    source "/etc/os-release"
+
+    # Format the OS version string
+    os_version="${NAME}_${VERSION_ID}"
+    # if NAME is Ubuntu, then prefix with x
+    if [ "${NAME}" == "Ubuntu" ]; then
+        os_version="x${os_version}"
+    fi
+
+    # Set the environment variable
+    OS="$os_version"
+else
+    # Set a default value if we can't read /etc/os-release
+	echo "Unknown OS: $NAME"
+    exit 1
+fi
